137e3a1032e79bcec821721f045b9de9afd4854a
wolffman122 <jon.wolff@wolfftech.com>
2017-12-05 17:04:34 -0500

    Holder changes
    Still need to find better way to repair containers.

--- src/processTypes/empireActions/lifetimes/holderBuilder.ts
+++ src/processTypes/empireActions/lifetimes/holderBuilder.ts
@@ -1,9 +1,7 @@
 import { LifetimeProcess } from "os/process";
-import { MoveProcess } from "processTypes/creepActions/move";
 import { BuildProcess } from "processTypes/creepActions/build";
 //import { Utils } from "lib/utils";
 import { CollectProcess } from "processTypes/creepActions/collect";
-import { HarvestProcess } from "processTypes/creepActions/harvest";
 //import { HarvestProcess } from "processTypes/creepActions/harvest";
 
 interface HoldBuilderLifetimeProcessMetaData
@@ -28,66 +26,58 @@ export class HoldBuilderLifetimeProcess extends LifetimeProcess
 
     if(flag.pos.roomName != creep.pos.roomName)
     {
-      this.fork(MoveProcess, 'move-' + creep.name, this.priority - 1, {
-        creep: creep.name,
-        pos: flag.pos,
-        range: 10
-      });
-
-      return;
-    }
-
-    if(_.sum(creep.carry) === 0)
-    {
-      if(this.kernel.data.roomData[creep.room.name].containers.length == this.kernel.data.roomData[creep.room.name].sources.length)
-      {
-        let targets = _.filter(this.kernel.data.roomData[creep.room.name].containers, (c: StructureContainer) => {
-          return (c.store.energy > 0);
-        })
-
-        let target = creep.pos.findClosestByPath(targets);
-
-        if(target)
-        {
-          this.fork(CollectProcess, 'collect-' + creep.name, this.priority - 1, {
-            creep: creep.name,
-            target: target.id,
-            resource: RESOURCE_ENERGY
-          });
-
-          return;
-        }
-      }
-      else
-      {
-        if( this.kernel.data.roomData[creep.room.name].sources)
-        {
-          let source = creep.pos.findClosestByRange( this.kernel.data.roomData[creep.room.name].sources);
-
-          this.fork(HarvestProcess, 'harvest-' + creep.name, this.priority - 1, {
-            creep: creep.name,
-            source: source.id
-          });
-
-          return;
-        }
-      }
-    }
-
-    let target = creep.pos.findClosestByRange(this.kernel.data.roomData[creep.room.name].constructionSites);
-
-    if(target)
-    {
-      this.fork(BuildProcess, 'build-' + creep.name, this.priority - 1, {
-        creep: creep.name,
-        site: target.id
-      });
-
-      return;
+      this.log('Go to the flag ' + creep.name);
+      creep.travelTo(flag);
     }
     else
     {
-      creep.say('spare');
-    }
+      if(_.sum(creep.carry) === 0)
+      {
+        if(this.kernel.data.roomData[creep.room.name].containers.length == this.kernel.data.roomData[creep.room.name].sources.length)
+        {
+          let targets = _.filter(this.kernel.data.roomData[creep.room.name].containers, (c: StructureContainer) => {
+            return (c.store.energy > 0);
+          })
+
+          let target = creep.pos.findClosestByPath(targets);
+
+          if(target)
+          {
+            this.fork(CollectProcess, 'collect-' + creep.name, this.priority - 1, {
+              creep: creep.name,
+              target: target.id,
+              resource: RESOURCE_ENERGY
+            });
+
+            return;
+          }
+        }
+        else
+        {
+          if( this.kernel.data.roomData[creep.room.name].sources)
+          {
+            let source = creep.pos.findClosestByRange( this.kernel.data.roomData[creep.room.name].sources);
+
+            creep.harvest(source);
+          }
+        }
+      }
+
+      let target = creep.pos.findClosestByRange(this.kernel.data.roomData[creep.room.name].constructionSites);
+
+      if(target)
+      {
+        this.fork(BuildProcess, 'build-' + creep.name, this.priority - 1, {
+          creep: creep.name,
+          site: target.id
+        });
+
+        return;
+      }
+      else
+      {
+        creep.say('spare');
+      }
+  }
   }
 }
--- src/processTypes/empireActions/lifetimes/holderHarvester.ts
+++ src/processTypes/empireActions/lifetimes/holderHarvester.ts
@@ -1,8 +1,4 @@
 import { LifetimeProcess } from "os/process";
-import { HarvestProcess } from "processTypes/creepActions/harvest";
-import { DeliverProcess } from "processTypes/creepActions/deliver";
-import { RepairProcess } from "processTypes/creepActions/repair";
-
 
 export class HoldHarvesterLifetimeProcess extends LifetimeProcess
 {
@@ -48,50 +44,16 @@ export class HoldHarvesterLifetimeProcess extends LifetimeProcess
         creep.travelTo(container);
       }
 
-      if(container.store.energy < container.storeCapacity)
+      creep.harvest(source);
+
+      if(container.hits <= container.hitsMax * .9 && _.sum(creep.carry) > 0)
       {
-        creep.harvest(source);
+        creep.repair(container);
+      }
+
+      if(container.store.energy < container.storeCapacity && _.sum(creep.carry) == creep.carryCapacity )
+      {
         creep.transfer(container, RESOURCE_ENERGY);
-      }
-      else
-      {
-        this.suspend = 10;
-      }
-
-    }
-    else
-    {
-      if(_.sum(creep.carry) === 0)
-      {
-        this.fork(HarvestProcess, 'harvest-' + creep.name, this.priority - 1, {
-          source: this.metaData.source,
-          creep: creep.name
-        })
-
-        return;
-      }
-
-      if(this.kernel.data.roomData[creep.room.name].sourceContainerMaps[this.metaData.source])
-      {
-        let container = this.kernel.data.roomData[creep.room.name].sourceContainerMaps[this.metaData.source];
-
-        if(container.hits < container.hitsMax)
-        {
-          this.fork(RepairProcess, 'repair-' + creep.name, this.priority - 1, {
-            creep: creep.name,
-            target: container.id
-          })
-          return;
-        }
-
-        if(_.sum(container.store) < container.storeCapacity)
-        {
-          this.fork(DeliverProcess, 'deliver-' + creep.name, this.priority - 1, {
-            target: container.id,
-            creep: creep.name,
-            resource: RESOURCE_ENERGY
-          })
-        }
       }
     }
   }
